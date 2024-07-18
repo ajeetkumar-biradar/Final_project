e57907ad10165fe6ec7b6872e82e738bc66e9389
Ajeetkumar Biradar <136881345+ajeetkumar-biradar@users.noreply.github.com>
2024-07-18 07:36:48 +0530

    Update settings.py

--- railway_enquiry_system/railway_enquiry_system/settings.py
+++ railway_enquiry_system/railway_enquiry_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-45wgd+z+%(!v3742tt&)n+zo#ro+cc&l@bp3k_v&7x$%zh5250
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.60.81.199']
 
 
 # Application definition
